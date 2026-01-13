c7320df8953c7c3a913ff17a65784f7d80461aaf
David van Hoek <turtleprogramsmail@gmail.com>
2026-01-13 10:27:49 +0100

    add docs

--- Module/TurtlAPI.js
+++ Module/TurtlAPI.js
@@ -50,6 +50,7 @@ export class TurtlAPI {
    * @param {string} config.host - The API host URL
    * @param {Function} [config.getAuthToken=null] - Optional function to retrieve authentication token
    * @param {boolean} [config.mock=false] - Whether to use mock mode
+   * @param {boolean} [config.defaultMockResultmock=false] - What mock result to use in default
    *
    * @description
    * Initializes the TurtlAPI module with:
